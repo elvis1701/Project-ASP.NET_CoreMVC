244c9ac55891adc893c6da86192a2bcf8fca8567
elvis.alves54@gmail.com <elvis.alves54@gmail.com>
2021-06-16 12:15:49 -0300

    Foreign Key

--- SalesWebMVC1/Servicos/SellerService.cs
+++ SalesWebMVC1/Servicos/SellerService.cs
@@ -23,6 +23,7 @@ namespace SalesWebMVC1.Servicos
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
